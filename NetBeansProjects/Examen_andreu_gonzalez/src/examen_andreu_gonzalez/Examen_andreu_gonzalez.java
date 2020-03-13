7057aa7dd4a488fcf87738eaaa4150198fed7644
Tu Nombre <alumne@profesor-HP-ProDesk-600-G1-SFF>
2020-03-13 08:43:41 +0100

    "Proyecto examen"

---
+++ NetBeansProjects/Examen_andreu_gonzalez/src/examen_andreu_gonzalez/Examen_andreu_gonzalez.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examen_andreu_gonzalez;
+
+/**
+ *
+ * @author alumne
+ */
+public class Examen_andreu_gonzalez {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Me llamo andreu gonzalez y estoy haciendo el examen de la UD4");
+    }
+    
+}
